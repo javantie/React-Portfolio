bc6806748de0bef09b987198c280dbd15fc63e9c
Akeem C <javantieclarke@gmail.com>
2021-10-12 02:44:01 -0400

    inital commit

---
+++ src/components/Nav/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <nav>
+      <ul className="flex-row">
+        <li className="mx-2">About Me</li>
+        <li className="mx-2">Contact Me</li>
+        <li className="mx-2">Resume</li>
+        <li className="mx-2">Projects</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
